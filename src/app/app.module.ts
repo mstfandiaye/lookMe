95ba4d413f5f95555f14e367fc9ea4833d31532a
mstfanadiye <mstfa.ndiaye23@gmail.com>
2024-05-11 19:23:03 +0000

    component created

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FaceSnapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
