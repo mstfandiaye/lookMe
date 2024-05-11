4eb9f2a062f72cb48c9785eaadfdf51c531115ff
mstfanadiye <mstfa.ndiaye23@gmail.com>
2024-05-11 19:31:11 +0000

    ngOninit

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+
+  title! : string;
+  description! : string;
+  creationDate! : Date ;
+  like! : number ;
+
+  ngOnInit(): void {
+
+    this.title = 'my friend' ;
+    this.description = 'best' ;
+    this.creationDate = new Date ;
+    this.like = 0 ;
+    
+  }
+
 
 }
