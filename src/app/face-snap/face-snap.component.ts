1a974efd3c98769081e826e1984e4248638b51f2
mstfanadiye <mstfa.ndiaye23@gmail.com>
2024-05-11 21:27:21 +0000

    data binding | attribut binding

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,13 +11,16 @@ export class FaceSnapComponent implements OnInit {
   description! : string;
   creationDate! : Date ;
   like! : number ;
+  imageurl! : string;
+
 
   ngOnInit(): void {
 
-    this.title = 'my friend' ;
-    this.description = 'best' ;
+    this.title = 'gift' ;
+    this.description = 'nature beauty' ;
     this.creationDate = new Date ;
     this.like = 0 ;
+    this.imageurl = 'https://cdn.pixabay.com/photo/2024/04/20/11/47/ai-generated-8708404_1280.jpg'
 
   }
 
